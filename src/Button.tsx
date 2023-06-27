13144d468252aab71465838e39554043b4157d34
Renato Napoli <renato.napoli@nagro.app>
2023-06-27 16:18:07 -0300

    Feat: eslint / React-router-dom / layout de rotas

--- src/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from "./Button.styles";
-
-interface ButttonProps {
-  variant?: ButtonVariant;
-}
-
-export function Button({ variant = "primary" }: ButttonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>;
-}
