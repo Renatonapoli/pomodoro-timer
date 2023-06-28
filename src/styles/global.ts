26248f023f82b27a570c7d4c40335ecb0d47eacd
Renato Napoli <renato.napoli@nagro.app>
2023-06-28 17:47:13 -0300

    Feat:  Home e todo seu css

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,6 +15,7 @@ export const GlobalStyle = createGlobalStyle`
 body {
   background: ${(props) => props.theme['gray-900']};
   color: ${(props) => props.theme['gray-300']};
+  -webkit-font-smoothing: antialiased
 }
 
 
@@ -22,5 +23,6 @@ body, input, textarea, button {
   font-family: 'Roboto', sans-serif;
   font-weight: 400;
   font-size: 1rem;
+  
 }
 `
